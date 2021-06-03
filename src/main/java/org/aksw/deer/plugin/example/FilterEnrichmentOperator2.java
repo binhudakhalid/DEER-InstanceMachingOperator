bd213d24d76ed4b48e8fb335f887eead9202bbe2
Khalid Bin Huda <khalid.bin.huda@hotmail.com>
2021-06-03 23:49:15 +0200

    create Limes config Object for unsupervise ML

--- src/main/java/org/aksw/deer/plugin/example/FilterEnrichmentOperator2.java
+++ src/main/java/org/aksw/deer/plugin/example/FilterEnrichmentOperator2.java
@@ -15,6 +15,8 @@ import org.aksw.limes.core.io.config.writer.RDFConfigurationWriter;
 import org.aksw.limes.core.io.config.writer.XMLConfigurationWriter;
 import org.aksw.limes.core.io.serializer.ISerializer;
 import org.aksw.limes.core.io.serializer.SerializerFactory;
+import org.aksw.limes.core.ml.algorithm.LearningParameter;
+import org.aksw.limes.core.ml.algorithm.MLImplementationType;
 import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.rdf.model.*;
 import org.apache.jena.vocabulary.VCARD;
@@ -106,9 +108,9 @@ public class FilterEnrichmentOperator2 extends AbstractParameterizedEnrichmentOp
 
 		System.out.println("Just running Limes");
 		createConfigurationFile();
-		//CallingLimes();
+		 //CallingLimes();
 		System.out.println("Just running Limes");
-		
+
 		// create an empty Model
 
 		// create the resource
@@ -158,146 +160,168 @@ public class FilterEnrichmentOperator2 extends AbstractParameterizedEnrichmentOp
 		}
 		return resultModel;
 	}
-	
-	
+
 	public void createConfigurationFile() {
-		
+
 		Configuration conf = new Configuration();
-	
-		//adding prefix
+
+		// adding prefix
 		conf.addPrefix("geom", "http://geovocab.org/geometry#");
-        conf.addPrefix("geos", "http://www.opengis.net/ont/geosparql#");
-        conf.addPrefix("lgdo", "http://linkedgeodata.org/ontology/");
-        conf.addPrefix("alie", "https://linkedgeodata.org/ontology/");
-        
-         
-        KBInfo src = new KBInfo();
-        
-        src.setId("linkedgeodata");
-        src.setEndpoint("http://linkedgeodata.org/sparql");
-        src.setVar("?x");
-        src.setPageSize(2000);
-        src.setRestrictions(
-                new ArrayList<String>(
-                        Arrays.asList(new String[]{"?x a lgdo:RelayBox"})
-                )
-        );
-        
-        System.out.println("Just running Limes 1");
-        src.setProperties(
-                Arrays.asList(new String[]{"geom:geometry/geos:asWKT RENAME polygon"})
-        );
-        conf.setSourceInfo(src);
-        
-        System.out.println("Just running Limes 2");
-        
-        KBInfo target = new KBInfo();
-        target.setId("linkedgeodata");
-        target.setEndpoint("http://linkedgeodata.org/sparql");
-        target.setVar("?y");
-        target.setPageSize(2000);
-        target.setRestrictions(
-                new ArrayList<String>(
-                        Arrays.asList(new String[]{"?x a lgdo:RelayBox"})
-                )
-        );
-        target.setProperties(
-                Arrays.asList(new String[]{"geom:geometry/geos:asWKT RENAME polygon"})
-        );
-        conf.setTargetInfo(target);
+		conf.addPrefix("geos", "http://www.opengis.net/ont/geosparql#");
+		conf.addPrefix("lgdo", "http://linkedgeodata.org/ontology/");
+		conf.addPrefix("alie", "https://linkedgeodata.org/ontology/");
 
-        conf.setMetricExpression("geo_hausdorff(x.polygon, y.polygon)");
+		conf.addPrefix("owl", "http://www.w3.org/2002/07/owl#");
+		conf.addPrefix("url", "http://schema.org/");
+		conf.addPrefix("rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#");
+		conf.addPrefix("dbpo", "http://dbpedia.org/ontology/");
+		conf.addPrefix("rdfs", "http://www.w3.org/2000/01/rdf-schema#");
 
-        conf.setAcceptanceFile("lgd_relaybox_verynear.nt");
-        conf.setAcceptanceThreshold(0.9);
-        conf.setAcceptanceRelation("lgdo:near");
+		KBInfo src = new KBInfo();
 
-        conf.setVerificationFile("lgd_relaybox_near.nt");
-        conf.setVerificationThreshold(0.5);
-        conf.setVerificationRelation("lgdo:near");
+		src.setId("sourceId");
+		src.setEndpoint("http://dbpedia.org/sparql");
+		src.setVar("?s");
+		src.setPageSize(2000);
+		src.setRestrictions(new ArrayList<String>(Arrays.asList(new String[] { "?s rdf:type url:Movie" })));
+		src.setProperties(Arrays.asList(new String[] { "rdfs:label" }));
 
-        conf.setExecutionEngine("default");
-        conf.setExecutionPlanner("default");
-        conf.setExecutionRewriter("default");
-        
-        System.out.println("Just running Limes 3");
-        
+		conf.setSourceInfo(src);
+		System.out.println("Just running Limes 1");
 
-        conf.setOutputFormat("TAB");
-        System.out.println("Just running Limes 4");
+		
+		System.out.println("Just running Limes 2");
 
-        RDFConfigurationWriter writer = new RDFConfigurationWriter();
-        System.out.println("Just running Limes 5" + writer.toString());
-        
-        
-        
-         
-        System.out.println(" yeh Allah2 : config.toString()" +  conf.toString());
+		KBInfo target = new KBInfo();
+		target.setId("targetId");
+		target.setEndpoint("http://dbpedia.org/sparql");
+		target.setVar("?t");
+		target.setPageSize(2000);
+		target.setRestrictions(new ArrayList<String>(Arrays.asList(new String[] { "?t rdf:type dbpo:Film" })));
+		target.setProperties(Arrays.asList(new String[] { "rdfs:label" }));
+		conf.setTargetInfo(target);
+		
 
-	    System.out.println(" yeh Allah2 : config.getVerificationThreshold()" +  conf.getVerificationThreshold());
-	    System.out.println(" yeh Allah2 : config.getMlAlgorithmName()" +  conf.getMlAlgorithmName());
-	    System.out.println(" yeh Allah2 please : config.getPrefixes()" +  conf.getPrefixes());
+		
+		//Set either Metric or MLALGORITHM
+		//conf.setMetricExpression("geo_hausdorff(x.polygon, y.polygon)");
 
-        
-        try {
-        	 System.out.println("Just running Limes 6");
-        	 writer.write(conf, "F:/Data/test10.ttl","TTL");
+	    MLImplementationType mlImplementationType = MLImplementationType.UNSUPERVISED;
+		conf.setMlAlgorithmName("wombat simple");
+		conf.setMlImplementationType(mlImplementationType);
+		
+		
+		LearningParameter learningParameter = new LearningParameter();
+		learningParameter.setName("max execution time in minutes");
+		learningParameter.setValue(60);
+		
+		
+		
+		
+		
+	    List<LearningParameter> mlAlgorithmParameters = new ArrayList<>();
+	    mlAlgorithmParameters.add(learningParameter);
+	    
+	    conf.setMlAlgorithmParameters(mlAlgorithmParameters);
+	    
+	    
 
-			 System.out.println("Just running Limes 7");
+		//list.add(new MyObject("First MyObject"));, 0
+
+	 //L/ist<LearningParameter> mlAlgorithmParameters = new ArrayList<>();
+
+		
+	 
+
+	 
+	
+	 
+		
+		
+		//Acceptance
+		conf.setAcceptanceThreshold(0.98);
+		conf.setAcceptanceFile("accepted.nt");
+		conf.setAcceptanceRelation("owl:sameAs");
+
+		//Review
+		conf.setVerificationThreshold(0.9);
+		conf.setVerificationFile("reviewme.nt");
+		conf.setVerificationRelation("owl:sameAs");
+
+		//EXECUTION
+		conf.setExecutionRewriter("default");
+		conf.setExecutionPlanner("default");
+		conf.setExecutionEngine("default");
+
+		System.out.println("Just running Limes 3");
+
+		conf.setOutputFormat("TAB");
+		System.out.println("Just running Limes 4");
+
+		RDFConfigurationWriter writer = new RDFConfigurationWriter();
+		System.out.println("Just running Limes 5" + writer.toString());
+
+		System.out.println(" yeh Allah2 : config.toString()" + conf.toString());
+
+		System.out.println(" yeh Allah2 : config.getVerificationThreshold()" + conf.getVerificationThreshold());
+		System.out.println(" yeh Allah2 : config.getMlAlgorithmName()" + conf.getMlAlgorithmName());
+		System.out.println(" yeh Allah2 please : config.getPrefixes()" + conf.getPrefixes());
+
+		try {
+			System.out.println("Just running Limes 6");
+			writer.write(conf, "F:/Data/test10.ttl", "TTL");
+
+			System.out.println("Just running Limes 7");
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-        
-       
-		
+
 	}
 
 	public void CallingLimes() {
 
-	/*	String LIMES_CONFIGURATION_FILE = "F://Newfolder//LIMES//t//fileOldML.xml";
-		String LIMES_OUTPUT_LOCATION = "F://Newfolder//LIMES//t";
-		ValidatableParameterMap parameters = getParameterMap();
+		/*
+		 * String LIMES_CONFIGURATION_FILE = "F://Newfolder//LIMES//t//fileOldML.xml";
+		 * String LIMES_OUTPUT_LOCATION = "F://Newfolder//LIMES//t";
+		 * ValidatableParameterMap parameters = getParameterMap();
+		 * 
+		 * String limesConfigurationFile =
+		 * parameters.get(LIMES_CONFIGURATION_FILE).asResource().getURI();
+		 */
 
-		String limesConfigurationFile = parameters.get(LIMES_CONFIGURATION_FILE).asResource().getURI();
-*/
-		
-		String hardCoded =  "F://Newfolder//LIMES//t//fileOldML.xml";
+		String hardCoded = "F://Newfolder//LIMES//t//fileOldML.xml";
 		String limesOutputLocation = "F://Newfolder//LIMES//t";
-		AConfigurationReader reader =  new XMLConfigurationReader(hardCoded.toString());
-	    Configuration config = reader.read();
-	    System.out.println(" yeh Allah : config.toString()" +  config.toString());
-	    System.out.println(" yeh Allah : config.getMlAlgorithmName()" +  config.getMlAlgorithmName());
-	    System.out.println(" yeh Allah : config.getVerificationThreshold()" +  config.getVerificationThreshold());
-	    
-	    
-	    
-	    String sourceEndpoint = config.getSourceInfo().getEndpoint();
-	    String targetEndpoint = config.getTargetInfo().getEndpoint();
-	    int limit = -1;
-	    LimesResult mappings = Controller.getMapping(config);
-	    String outputFormat = config.getOutputFormat();
-	    ISerializer output = SerializerFactory.createSerializer(outputFormat);
-	    
-	    output.setPrefixes(config.getPrefixes());
-	    
-	    String workingDir = limesOutputLocation;//"F:\\Newfolder\\LIMES\\t";
-	    File verificationFile = new File(workingDir, config.getVerificationFile());
-	    File acceptanceFile = new File(workingDir, config.getAcceptanceFile());
-	    
-	    output.writeToFile(mappings.getVerificationMapping(), config.getVerificationRelation(),
-	            verificationFile.getAbsolutePath());
-	    output.writeToFile(mappings.getAcceptanceMapping(), config.getAcceptanceRelation(),
-	            acceptanceFile.getAbsolutePath()); 
-	    System.out.println("100 ==HD== ");
-	    
-	    
-	    
-	    
-	    
+		AConfigurationReader reader = new XMLConfigurationReader(hardCoded.toString());
+		Configuration config = reader.read();
+		System.out.println(" yeh Allah : config.toString()" + config.toString());
+		System.out.println(" yeh Allah : config.getMlAlgorithmName()" + config.getMlAlgorithmName());
+		System.out.println(" yeh Allah : config.getVerificationThreshold()" + config.getVerificationThreshold());
+
+		System.out.println(" yeh Allah : config.getMlAlgorithmParameters() khd: " + config.getMlAlgorithmParameters());
+		System.out.println(" yeh Allah : config.getMlAlgorithmParameters()toString khd: " + config.getMlAlgorithmParameters().toString());
+		System.out.println(" yeh Allah : config.getMlAlgorithmParameters()toString khd: " + config.getMlAlgorithmParameters().indexOf(0));
 		
 		
-		
+		String sourceEndpoint = config.getSourceInfo().getEndpoint();
+		String targetEndpoint = config.getTargetInfo().getEndpoint();
+		int limit = -1;
+		LimesResult mappings = Controller.getMapping(config);
+		String outputFormat = config.getOutputFormat();
+		ISerializer output = SerializerFactory.createSerializer(outputFormat);
+
+		output.setPrefixes(config.getPrefixes());
+
+		String workingDir = limesOutputLocation;// "F:\\Newfolder\\LIMES\\t";
+		File verificationFile = new File(workingDir, config.getVerificationFile());
+		File acceptanceFile = new File(workingDir, config.getAcceptanceFile());
+
+		output.writeToFile(mappings.getVerificationMapping(), config.getVerificationRelation(),
+				verificationFile.getAbsolutePath());
+		output.writeToFile(mappings.getAcceptanceMapping(), config.getAcceptanceRelation(),
+				acceptanceFile.getAbsolutePath());
+		System.out.println("100 ==HD== ");
+
 	}
 
 }
