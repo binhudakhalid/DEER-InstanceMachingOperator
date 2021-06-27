1cba2d9897971cd6096fccea603b9243b58aef1b
Khalid Bin Huda <khalid.bin.huda@hotmail.com>
2021-06-28 01:58:42 +0200

    working someWhat

--- src/main/java/org/aksw/deer/plugin/example/IntanceMactchingOperator.java
+++ src/main/java/org/aksw/deer/plugin/example/IntanceMactchingOperator.java
@@ -122,10 +122,8 @@ public class IntanceMactchingOperator extends AbstractParameterizedEnrichmentOpe
 		conf.addPrefix("alie", "https://linkedgeodata.org/ontology/");
 
 		conf.addPrefix("owl", "http://www.w3.org/2002/07/owl#");
-		conf.addPrefix("url", "http://schema.org/");
-		
-		conf.addPrefix("dbpo", "http://dbpedia.org/ontology/");
-		conf.addPrefix("rdfs", "http://www.w3.org/2000/01/rdf-schema#");
+		conf.addPrefix("rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#");
+		 
 
 		KBInfo src = new KBInfo();
 
