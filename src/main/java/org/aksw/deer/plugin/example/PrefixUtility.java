7fe40bd75821191ec2017979bac8b7787ee0e686
Khalid Bin Huda <khalid.bin.huda@hotmail.com>
2021-09-05 14:21:36 +0200

    before work

---
+++ src/main/java/org/aksw/deer/plugin/example/PrefixUtility.java
@@ -0,0 +1,25 @@
+package org.aksw.deer.plugin.example;
+
+import java.util.HashMap;
+
+public class PrefixUtility {
+	
+	
+	private HashMap<String,String> extractPrefixFromFile() {
+		
+		HashMap<String, String> TempHashMap = null;
+		
+		return TempHashMap;
+		
+	}
+	
+	
+	public void removeUnecessayPrefix(String sparqlQuery) {
+		
+	}
+	
+	
+	
+	
+
+}
