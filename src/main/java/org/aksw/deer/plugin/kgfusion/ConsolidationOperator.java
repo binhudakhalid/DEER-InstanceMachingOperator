8809dda6a554004f4169b4f3785f1d2f6d5f2d73
Khalid Bin Huda <khalid.bin.huda@hotmail.com>
2022-01-23 12:53:53 +0100

    creating ouput file

--- src/main/java/org/aksw/deer/plugin/kgfusion/ConsolidationOperator.java
+++ src/main/java/org/aksw/deer/plugin/kgfusion/ConsolidationOperator.java
@@ -1,5 +1,9 @@
 package org.aksw.deer.plugin.kgfusion;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
 import java.util.List;
 
 import org.aksw.deer.enrichments.AbstractParameterizedEnrichmentOperator;
@@ -11,6 +15,8 @@ import org.apache.jena.rdf.model.RDFNode;
 import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.rdf.model.Statement;
 import org.apache.jena.rdf.model.StmtIterator;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFDataMgr;
 import org.pf4j.Extension;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,15 +37,27 @@ public class ConsolidationOperator extends AbstractParameterizedEnrichmentOperat
 				.declareValidationShape(getValidationModelFor(ConsolidationOperator.class)).build();
 	}
 
+	@SuppressWarnings("null")
 	@Override
 	protected List<Model> safeApply(List<Model> models) { // 3
 
 	//	System.out.println("shah" + "The output from Instance Matching Operator models.get(0) " + models.size() ); 
 
 //		System.out.println("The output from Instance Matching Operator models.get(0) " + models.get(0).size() ); 
-		System.out.println(" aop The output from Instance Matching Operator models " + models ); 
+		System.out.println(" aop The output from Instance Matching Operator models " + models.get(0) ); 
 		System.out.println(" end aop "); 
 		
+		
+		FileWriter writer;
+		try {
+			writer = new FileWriter("outputFromInstanceMatching.ttl");
+			models.get(0).write(writer, "N-TRIPLE");
+
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	   
 
 		//System.out.println("The output from Instance Matching Operator models.get(1) " + models.get(1) ); 
 
